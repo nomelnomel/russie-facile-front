b93cfb5784d8042728ed5f7094cc5272e1590fde
Ilya Polivin <nomelnomel@gmail.com>
2021-09-27 22:28:18 +0300

    added missing files

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  head: {
+    title: 'front',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;1,400&display=swap',
+      },
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/styles/globals.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '@/plugins/vue-awesome-swiper', mode: 'client' },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/strapi'
+  ],
+  strapi: {
+    url: process.env.API_URL || "http://localhost:1337",
+    entities: [
+      'towns',
+      'tours',
+      'excursions'
+    ],
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    main page
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+h2{
+  font-size: 20px;
+  span{
+    font-size: 30px;
+  }
+}
+</style>
